8ff47d4d4d7663f15a0e60939216a52abc46c6ac
Shanuka Dilshan <shanukadid@gmail.com>
2024-03-14 15:24:51 +0530

    Initialized system 3/14

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./elements/Home";
-import Create from "./elements/Create";
-import Edit from "./elements/Edit";
-import Read from "./elements/Read";
+// client/src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/read/:id" element={<Read />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,78 +1,17 @@
-/*
-const express = require('express')
-const mysql = require('mysql')
-const cors = require('cors')
-const path = require('path')
-
-
-const app = express()
-
-app.use(express.static(path.join(__dirname,"public")))
-app.use(cors())
-app.use(express.json())
-
-const port = 3000
-
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "visionedu"
-})
-
-app.post('/add_user', (req, res)=>{
-    sql = "INSERT INTO user (firstname, email, lastname, gender) VALUES (?, ?, ?, ?)";
-    const values = {
-        req.body.name,
-        req.body.email,
-        req.body.age,
-        req.body.gender
-    }
-    db.query(sql,values, (err, result)=>{
-        if(err) return res.json({message: 'Something unexpected has occured' + err})
-        return res.json({success: "Student added sucessfully"})
-    })
-})
-
-
-app.listen(port, ()=>{
-    console.log('listening')
-})
-*/
-
-
-
 const express = require('express');
-const mysql = require('mysql');
 const cors = require('cors');
-const path = require('path');
+const db = require('./db'); // Require the database connection setup
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
-app.use(express.json());
-
 const port = 5000;
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "visionedu"
-});
+// Middleware
+app.use(express.json());
+app.use(cors());
 
-app.post("/add_user", (req, res) => {
-    const sql =
-      "INSERT INTO user (userid, firstname, email, gender) VALUES (?, ?, ?, ?);";
-    const values = [ req.body.age, req.body.name, req.body.email, req.body.gender];
-    db.query(sql, values, (err, result) => {
-      if (err)
-        return res.json({ message: "Something unexpected has occured" + err });
-      return res.json({ success: "Student added successfully" });
-    });
-});
+// Your routes and other server logic go here
 
+// Start server
 app.listen(port, () => {
-    console.log('Listening on port', port);
+    console.log(`Server is running on port ${port}`);
 });
