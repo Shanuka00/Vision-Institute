581f7d4ab8f81587e30b6356e5759533be5ba9ad
Shanuka Dilshan <shanukadid@gmail.com>
2024-03-19 18:22:17 +0530

    Web site initialized 3/19

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,115 +1,69 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import loginImage from '../images/login_image.png';
+import visionLogoT from '../images/visionLogoT.png';
 import Nav from 'react-bootstrap/Nav';
 
 function Login() {
   return (
-    <section className="h-screen mx-40 w-70 space-y-20 mt-20 mb-20 bg-gray-200 rounded-xl p-10">
+    
+    <section className="mx-40 w-70 space-y-20 mt-10 pt-1 mb-10 bg-gray-200 rounded-xl p-10">
       <div className="h-full">
-        <div
-          className="flex h-full flex-wrap items-center justify-center lg:justify-between">
-          <div
-            className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+
+        <div className="flex h-full flex-wrap items-center justify-center lg:justify-between">
+          <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12 pl-10 mt-20">    
             <img
-              src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              src={loginImage}
               className="w-full"
-              alt="Sample bla bla bla" />
+              alt="A classroom" />  
+
+            <div className="text-center">
+              <label className="inline-block text-lg font-bold italic text-blue-900 hover:cursor-pointer mt-4">
+                "දක්ෂ පලපුරුදු ගුරු මඩුල්ලක්<br/>ඔබ ප්‍රදේශයේ<br/>ඔබත් සම්බන්ධ වන්න"
+              </label>
+            </div>
+
+
           </div>
 
-          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+
+          <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12 pr-10">
             <form>
-              <div
-                className="flex flex-row items-center justify-center lg:justify-start">
-                <p className="mb-0 me-4 text-lg">Sign in with</p>
 
-                <button
-                  type="button"
-                  data-twe-ripple-init
-                  data-twe-ripple-color="light"
-                  className=" mx-1 inline-block h-9 w-9 rounded-full bg-primary fill-white p-2 uppercase leading-normal shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+            <div className="text-center mb-3 w-40 ml-auto mr-auto mt-2">
+              <Nav.Link as={Link} to="/">
+                <img src={visionLogoT} alt="Vision Logo" />
+              </Nav.Link>
+            </div>
 
-                  <span className="[&>svg]:mx-auto [&>svg]:h-3.5 [&>svg]:w-3.5">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 320 512">
-                      
-                      <path
-                        d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
-                    </svg>
-                  </span>
-                </button>
-
-                
-                <button
-                  type="button"
-                  data-twe-ripple-init
-                  data-twe-ripple-color="light"
-                  className=" mx-1 inline-block h-9 w-9 rounded-full bg-primary fill-white p-2 uppercase leading-normal shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
-                  
-                  <span className="[&>svg]:mx-auto [&>svg]:h-3.5 [&>svg]:w-3.5">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512">
-                      <path
-                        d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
-                    </svg>
-                  </span>
-                </button>
-
-                <button
-                  type="button"
-                  data-twe-ripple-init
-                  data-twe-ripple-color="light"
-                  className=" mx-1 inline-block h-9 w-9 rounded-full bg-primary fill-white p-2 uppercase leading-normal shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
-                  <span className="[&>svg]:mx-auto [&>svg]:h-3.5 [&>svg]:w-3.5">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 448 512">
-                      
-                      <path
-                        d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
-                    </svg>
-                  </span>
-                </button>
+              <div>
+                <Link to="/std_registration">
+                  <button className="inline-block w-full rounded bg-primary px-7 pb-3 pt-3 text-sm font-medium leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                  👉 New Registration 👈<br/>(අලුතින් ලියාපදිංචි වන්න)</button>
+                </Link>
               </div>
 
-              <div
-                className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 dark:before:border-neutral-500 dark:after:border-neutral-500">
-                <p
-                  className="mx-4 mb-0 text-center font-semibold dark:text-white">
-                  Or
-                </p>
-              </div>
+                <div className="relative mb-6 bg-white rounded mt-4" data-twe-input-wrapper-init>
+                  <input
+                    type="text"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-black dark:text-black dark:placeholder:text-gray-500 dark:autofill:shadow-autofill dark:peer-focus:text-primary"
+                    id="exampleFormControlInput2"
+                    placeholder="Vision ID"
+                  />
+                </div>
 
-              <div className="relative mb-6" data-twe-input-wrapper-init>
-                <input
-                  type="text"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlInput2"
-                  placeholder="Email address" />
-                <label
-                  htmlFor="exampleFormControlInput2"
-                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[1.15rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
-                  >Email address
-                </label>
-              </div>
+                <div className="relative mb-6 bg-white rounded mt-4" data-twe-input-wrapper-init>
+                  <input
+                    type="password"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-black dark:text-black dark:placeholder:text-gray-500 dark:autofill:shadow-autofill dark:peer-focus:text-primary"
+                    id="exampleFormControlInput22"
+                    placeholder="Password"
+                  />
+                </div>
 
-              
-              <div className="relative mb-6" data-twe-input-wrapper-init>
-                <input
-                  type="password"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-                  id="exampleFormControlInput22"
-                  placeholder="Password" />
-                <label
-                  htmlFor="exampleFormControlInput22"
-                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[1.15rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
-                  >Password
-                </label>
-              </div>
 
-              <div className="mb-6 flex items-center justify-between">
+              <div className="mb-3 flex items-center justify-between">
                
                 <div className="mb-[0.125rem] block min-h-[1.5rem] ps-[1.5rem]">
                   <input
@@ -132,29 +86,25 @@ function Login() {
               <div className="text-center lg:text-left">
                 <button
                   type="button"
-                  className="inline-block w-full rounded bg-primary px-7 pb-2 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                  className="inline-block w-full rounded bg-primary mt-0 px-7 pb-3 pt-3 text-base font-medium leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
                   data-twe-ripple-init
                   data-twe-ripple-color="light">
                   Login
                 </button>
 
-               
-                <p className="mb-0 mt-2 pt-1 text-sm font-semibold">
-                  Don't have an account?
+                <div>
+                  <Link to="/std_registration">
+                    <button className="inline-block w-full rounded bg-primary mt-4 px-7 pb-3 pt-3 text-sm font-medium leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                    If you are already a student attending<br/>classes in the institute<br/>(ඔබ දැනටමත් ආයතනයේ පන්ති<br/>පැමිණෙන සිසුවෙක් නම්)</button>
+                  </Link>
+                </div>
 
-                  <Nav.Link as={Link} to="/std_registration">
-                    <p className="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700">Register</p>
-                  </Nav.Link>
-                  
-                </p>
               </div>
             </form>
           </div>
         </div>
       </div>
     </section>
-
-
 
   );
 }
