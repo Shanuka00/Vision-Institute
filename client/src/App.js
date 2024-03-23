b5324e66e5681dd0676e64a48caf4e8a6b9baa8a
Shanuka Dilshan <shanukadid@gmail.com>
2024-03-23 16:37:42 +0530

    Web site almost finished 3/23

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,41 @@
 // client/src/App.js
 
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// //import Home from './pages/home';
-// import Login from './pages/login';
-// import StudentRegistration from './pages/std_registration';
-
-// import Navbar from "./pages/components/Navbar";
-// import { Hero } from "./pages/components/Hero";
-// import About from "./pages/components/About";
-// import Support from "./pages/components/Support";
-// import AllInOne from "./pages/components/AllInOne";
-// import Pricing from "./pages/components/Pricing";
-// import Footer from "./pages/components/Footer";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-
-//         {" "}
-//         <Navbar />
-//         <Hero />
-//         <About />
-//         <Support />
-//         <AllInOne />
-//         <Pricing />
-//         <Footer />
-
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/std_registration" element={<StudentRegistration />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import StudentRegistration from './pages/std_registration';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import Navbar from "./pages/home/Navbar";
-import { Hero } from "./pages/home/Hero";
+import { VHome } from "./pages/home/VHome";
 import About from "./pages/home/About";
-import Support from "./pages/home/Support";
-import AllInOne from "./pages/home/AllInOne";
-import Pricing from "./pages/home/Pricing";
+import Teachers from "./pages/home/Teachers";
+import News from "./pages/home/News";
+import Contactus from "./pages/home/Contactus";
 import Footer from "./pages/home/Footer";
 
 function App() {
   return (
+    <HelmetProvider>
     <Router>
       <Routes>
         <Route path="/" element={<>
           <Navbar />
-          <Hero />
+          <VHome />
           <About />
-          <Support />
-          <AllInOne />
-          <Pricing />
+          <Teachers />
+          <News />
+          <Contactus />
           <Footer />
         </>} />
         <Route path="/login" element={<Login />} />
         <Route path="/std_registration" element={<><Navbar /><StudentRegistration /></>} />
       </Routes>
+      <Helmet>
+          <title>Vision Institute - Atabage</title>
+      </Helmet>
     </Router>
+    </HelmetProvider>
   );
 }
 
