f3c096ab36c5ad6476f9b0632ff884537506e74d
Shanuka Dilshan <shanukadid@gmail.com>
2024-04-06 21:30:53 +0530

    Website contact us front done 4/06

--- client/src/pages/home/Footer.jsx
+++ client/src/pages/home/Footer.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="w-full mt-24 bg-indigo-950 text-white py-y px-2 pb-0">
+    <div className="w-full mt-12 bg-indigo-950 text-white py-y px-2 pb-0">
       <div className="pt-10 flex flex-col max-w-[1240px] px-2 py-3 m-auto justify-between sm:flex-row text-center text-gray-300 pb-0">
         <p className="py-4">Copyright © 2024 | Concept and Design by Shanuka Dilshan </p>
         <div className="flex justify-between sm:w-[100px] pt-4 text-2xl">
